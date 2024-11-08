f9d49371c561e2b386410ae8c67a05745b528524
HabitUDev <habitu2024@gmail.com>
2024-11-07 21:43:23 -0500

    Update route.ts

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -1,14 +1,11 @@
-import { createClerkClient } from '@clerk/backend';
+import { createClerkClient } from '@clerk/nextjs/server';
 
 const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY });
 
 export async function GET() {
   try {
-    // Fetch the user list using Clerk backend
     const userList = await clerkClient.users.getUserList();
-    
-    // Return the list as JSON response
-    return new Response(JSON.stringify(userList), { status: 200 });
+    return new Response(JSON.stringify(userList.data), { status: 200 });
   } catch (error) {
     console.error('Error fetching user list:', error);
     return new Response('Internal Server Error', { status: 500 });
