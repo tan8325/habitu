18d9bc1e182f19067dca9b9bb28296f072e15f69
Tan Bach <58007188+tan8325@users.noreply.github.com>
2024-11-08 10:17:11 -0500

    Update heroes.tsx

--- app/(landing)/_components/heroes.tsx
+++ app/(landing)/_components/heroes.tsx
@@ -1,24 +1,15 @@
 import Image from "next/image";
 
 export const Heroes = () => {
-  return (
-    <div className="flex items-center justify-center">
-      <Image
-        src="/desktop-light.png"
-        alt="laptop"
-        loading="lazy"
-        width="1000"
-        height="500"
-        className="hidden dark:block"
-      />
-      <Image
-        src="/desktop-dark.png"
-        alt="laptop dark"
-        loading="lazy"
-        width="1000"
-        height="500"
-        className="block dark:hidden"
-      />
-    </div>
-  );
+    return (
+        <div className="flex items-center justify-center">
+          <Image
+            src="/desktop.png"
+            alt="laptop"
+            loading="lazy"
+            width="1000"
+            height="500"
+          />
+        </div>
+  )
 };
